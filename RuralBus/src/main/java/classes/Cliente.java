13d73cbf3c5b164b1fe3d86fc0e3aee2094d720c
Caio R <114326411+Caio2909@users.noreply.github.com>
2024-11-25 17:07:28 -0300

    Add files via upload
    adicionadas classes, projeto mudado para maven

---
+++ RuralBus/src/main/java/classes/Cliente.java
@@ -0,0 +1,74 @@
+package classes;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String CPF;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String email, String senha, String CPF) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+}
---
+++ RuralBus/src/main/java/classes/Passagem.java
@@ -0,0 +1,54 @@
+package classes;
+
+public class Passagem {
+    String ID;
+    Cliente cliente;
+    Viagem viagem;
+    Assento assento;
+
+    public Passagem(Cliente cliente, Viagem viagem, Assento assento) {
+        this.cliente = cliente;
+        this.viagem = viagem;
+        this.assento = assento;
+
+        /*
+         * Inicializa o Assento da passagem
+         */
+
+        this.assento.ocupar();
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Viagem getViagem() {
+        return viagem;
+    }
+
+    public void setViagem(Viagem viagem) {
+        this.viagem = viagem;
+
+    }
+
+    public Assento getAssento() {
+        return assento;
+    }
+
+    public void setAssento(Assento assento) {
+        this.assento = assento;
+    }
+
+}
